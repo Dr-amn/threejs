987108424a85f2b8474d449ae72ce8ccf187aad7
Dramn <cepowiczjakub@gmail.com>
2025-02-11 00:29:39 +0100

    texturing-start

--- src/script.js
+++ src/script.js
@@ -15,18 +15,61 @@ const canvas = document.querySelector('canvas.vite')
 // Scene
 const scene = new THREE.Scene()
 
-
-
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+    // Floor
+    const floorAlphaTexture = textureLoader.load('./floor/alpha.jpg')
+    const floorColorTexture = textureLoader.load('./floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_diff_1k.webp')
+    const floorARMTexture = textureLoader.load('./floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_arm_1k.webp')
+    const floorNormalTexture = textureLoader.load('./floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_nor_gl_1k.webp')
+    const floorDisplacementTexture = textureLoader.load('./floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_disp_1k.webp')
+    // Floor Repeats
+        // Floor Color
+        floorColorTexture.colorSpace = THREE.SRGBColorSpace
+        floorColorTexture.repeat.set(8, 8)
+        floorColorTexture.wrapS = THREE.RepeatWrapping
+        floorColorTexture.wrapT = THREE.RepeatWrapping
+        // Floor ARM
+        floorARMTexture.repeat.set(8, 8)
+        floorARMTexture.wrapS = THREE.RepeatWrapping
+        floorARMTexture.wrapT = THREE.RepeatWrapping
+        // Floor Normal
+        floorNormalTexture.repeat.set(8, 8)
+        floorNormalTexture.wrapS = THREE.RepeatWrapping
+        floorNormalTexture.wrapT = THREE.RepeatWrapping
+        // Floor Displacement
+        floorDisplacementTexture.repeat.set(8, 8)
+        floorDisplacementTexture.wrapS = THREE.RepeatWrapping
+        floorDisplacementTexture.wrapT = THREE.RepeatWrapping
 /**
  * House
  */
-// FLOOR
+// Floor
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(20,20),
-    new THREE.MeshStandardMaterial()
+    new THREE.PlaneGeometry(20,20, 64, 64),
+    new THREE.MeshStandardMaterial({
+        alphaMap: floorAlphaTexture,
+        transparent: true,
+        map: floorColorTexture,
+        aoMap: floorARMTexture,
+        roughnessMap: floorARMTexture,
+        metalnessMap: floorARMTexture,
+        normalMap: floorNormalTexture,
+        displacementMap: floorDisplacementTexture,
+        displacementScale: 0.688,
+        displacementBias:-0.392
+    })
 )
 floor.rotation.x = - Math.PI * 0.5
 scene.add(floor)
+
+/**
+ * GUI
+ */
+gui.add(floor.material, 'displacementScale', -1, 1, 0.001).name('floorDisplacementScale')
+gui.add(floor.material, 'displacementBias', -1, 1, 0.001).name('floorDisplacementBias')
 
 // HOUSE GROUP
 const houseGroup = new THREE.Group()
@@ -47,8 +90,61 @@ scene.add(houseGroup)
     roof.position.y = 1.5 / 2 + 2.5
     roof.rotation.y = Math.PI * 0.25
     houseGroup.add(roof)
+    // Door
+    const door = new THREE.Mesh(
+        new THREE.PlaneGeometry(2.2, 2.2),
+        new THREE.MeshStandardMaterial({color: 0xff0000})
+    )
+    door.position.z = 2.001
+    door.position.y = 1.1
+    houseGroup.add(door)
+    // Bushes
+    const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
+    const bushMaterial = new THREE.MeshStandardMaterial()
+        // Bush One
+        const bushOne = new THREE.Mesh(bushGeometry, bushMaterial)
+        bushOne.scale.set(0.5, 0.5, 0.5)
+        bushOne.position.set(0.8, 0.2, 2.2)
+        // Bush Two
+        const bushTwo = new THREE.Mesh(bushGeometry, bushMaterial)
+        bushTwo.scale.set(0.25, 0.25, 0.25)
+        bushTwo.position.set(1.4, 0.1, 2.1)
+        // Bush Three
+        const bushThree = new THREE.Mesh(bushGeometry, bushMaterial)
+        bushThree.scale.set(0.4, 0.4, 0.4)
+        bushThree.position.set(-0.8, 0.1, 2.2)
+        // Bush Four
+        const bushFour = new THREE.Mesh(bushGeometry, bushMaterial)
+        bushFour.scale.set(0.15, 0.15, 0.15)
+        bushFour.position.set(-1, 0.05, 2.6)
 
+        houseGroup.add(bushOne, bushTwo, bushThree, bushFour)
 
+//Graves
+const gravesGroup = new THREE.Group()
+scene.add(gravesGroup)
+
+const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2)
+const graveMaterial = new THREE.MeshStandardMaterial()
+    // Graves loop
+    for(let i = 0; i < 30; i++){
+        const graveCircle = Math.random() * Math.PI * 2
+        const radius = 3 + Math.random() * 5
+        const x = Math.sin(graveCircle) * radius
+        const z = Math.cos(graveCircle) * radius
+
+        // Mesh
+        const grave = new THREE.Mesh(graveGeometry, graveMaterial)
+        grave.position.x = x
+        grave.position.z = z
+        grave.position.y = Math.random() * 0.4
+
+        grave.rotation.x = (Math.random() - 0.5) * 0.4
+        grave.rotation.y = (Math.random() - 0.5) * 0.4
+        grave.rotation.z = (Math.random() - 0.5) * 0.4
+        
+        gravesGroup.add(grave)
+    }
 
 /**
  * Lights
