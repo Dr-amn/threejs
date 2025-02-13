8f7033b45f57b20d1db54d90bbfe1e16e92b3458
Dramn <cepowiczjakub@gmail.com>
2025-02-13 23:37:09 +0100

    galaxy-almost-done

--- src/script.js
+++ src/script.js
@@ -1,121 +1,126 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
-import gsap from 'gsap'
-
-// DEBUG
-const gui = new GUI({
-    width: 300,
-    title: 'GUI'
-    // closeFolders: true
-})
-// gui.close()
-// gui.hide()
-
-window.addEventListener('keydown', (e) => {
-    if(e.key == 'g'){
-        gui.show(gui._hidden)
-    }
-})
-
-const debugObject = {}
-
-
-
-
-
-// TEXTURES
-const loadingManager = new THREE.LoadingManager()
-
-// loadingManager.onStart = () =>{
-//     console.log('onStart')
-// }
-// loadingManager.onLoad = () =>{
-//     console.log('onLoad')
-// }
-// loadingManager.onProgress = () =>{
-//     console.log('onProgress')
-// }
-// loadingManager.onError = () =>{
-//     console.log('onError')
-// }
-
-const textureLoader = new THREE.TextureLoader(loadingManager)
-
-// COLOR
-const doorColorTexture = textureLoader.load('/textures/door/basecolor.jpg')
-    doorColorTexture.colorSpace = THREE.SRGBColorSpace
-    doorColorTexture.rotation = Math.PI * 0.25
-    doorColorTexture.center.x = 0.5
-    doorColorTexture.center.y = 0.5
-
-
-// // ALPHA
-// const doorAlphaTexture = textureLoader.load('/textures/door/opacity.jpg')
-//     doorAlphaTexture.colorSpace = THREE.SRGBColorSpace
-// // HEIGHT
-// const doorHeightTexture = textureLoader.load('/textures/door/height.png')
-//     doorHeightTexture.colorSpace = THREE.SRGBColorSpace
-// // NORMAL
-// const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
-//     doorNormalTexture.colorSpace = THREE.SRGBColorSpace
-// // ROUGHNESS
-// const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
-//     doorRoughnessTexture.colorSpace = THREE.SRGBColorSpace
-// // AMBIENT OCCLUSION
-// const doorAmbientOcclusion = textureLoader.load('/textures/door/ambientOcclusion.jpg')
-// doorAmbientOcclusion.colorSpace = THREE.SRGBColorSpace
-
-
-
 
 /**
  * Base
  */
+// Debug
+const gui = new GUI({
+    width: 600
+})
+
 // Canvas
 const canvas = document.querySelector('canvas.vite')
+
 // Scene
 const scene = new THREE.Scene()
 
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
 
-
-//OBJECT
-    /**COLOR**/debugObject.color = "#42c286"
-
-const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-// console.log(geometry.attributes.uv)
-// const material = new THREE.MeshBasicMaterial({color: debugObject.color, wireframe: true})
-const material = new THREE.MeshBasicMaterial({map: doorColorTexture})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-
-
-
-const cubeGUI = gui.addFolder('Simple_cube')
-// cubeGUI.close()
-
-// DEBUG
-cubeGUI.add(mesh.position, 'y', -3, 3, 0.01).name('elevation')
-cubeGUI.add(mesh, 'visible')
-cubeGUI.add(material, 'wireframe')
-// cubeGUI.addColor(debugObject, 'color')
-//     .onFinishChange(()=>
-//     {
-//         material.color.set(debugObject.color)
-//     })
-
-debugObject.spin = () =>{
- gsap.to(mesh.rotation, { y: mesh.rotation.y + Math.PI * 2 })
+/**
+ * Galaxy
+ */
+const parameters = {
+    count: 100000,
+    size: 0.001,
+    radius: 5,
+    branches: 3,
+    spin: 1,
+    randomness: 0.02,
+    randomnessPower: 3,
+    insideColor: '#ff6030',
+    outsideColor: '1b3984'
 }
-cubeGUI.add(debugObject, 'spin')
 
-// debugObject.subdivision = 2
-// cubeGUI.add(debugObject, 'subdivision', 1, 20, 1)
-//     .onFinishChange(()=>{
-//         mesh.geometry.dispose()
-//         mesh.geometry = new THREE.BoxGeometry(1, 1, 1, debugObject.subdivision, debugObject.subdivision, debugObject.subdivision)
-//     })
+let galaxyGeometry = null
+let galaxyMaterial = null
+let points = null
 
+const generateGalaxy = () =>{
+    //Destroy
+    if(points !== null){
+        galaxyGeometry.dispose()
+        galaxyMaterial.dispose()
+        scene.remove(points)
+    }
+
+    //Geometry
+    galaxyGeometry = new THREE.BufferGeometry()
+    const positions = new Float32Array(parameters.count * 3)
+    const colors = new Float32Array(parameters.count * 3)
+
+    const colorInside = new THREE.Color(parameters.insideColor)
+    const colorOutside = new THREE.Color(parameters.outsideColor)
+
+        for(let i = 0; i < parameters.count; i++){
+            const i3 = i * 3
+            //POSITION
+            const galaxyRadius = Math.random() * parameters.radius
+            const spinAngle = galaxyRadius * parameters.spin
+            const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
+                //Randomness
+                const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1)
+                const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1)
+                const randomZ = Math.pow(Math.random(), parameters.randomnessPower)* (Math.random() < 0.5 ? 1 : -1)
+                // X
+                positions[i3 + 0] = Math.cos(branchAngle + spinAngle) * galaxyRadius + randomX
+                // Y
+                positions[i3 + 1] = randomY
+                // Z
+                positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * galaxyRadius + randomZ
+            
+            //COLORS
+                // R
+                colors[i3] = 1
+                // G
+                colors[i3 + 1] = 0
+                // B
+                colors[i3 + 2] = 0
+
+        }
+
+    galaxyGeometry.setAttribute(
+        'position',
+        new THREE.BufferAttribute(positions, 3)
+    )
+
+    galaxyGeometry.setAttribute(
+        'color',
+        new THREE.BufferAttribute(colors, 3)
+    )
+
+    //Material
+    galaxyMaterial = new THREE.PointsMaterial({
+        size: parameters.size,
+        sizeAttenuation: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending,
+        color: '#ff5588',
+        vertexColors: true
+    })
+
+    //Points
+    points = new THREE.Points(galaxyGeometry, galaxyMaterial)
+    scene.add(points)
+}
+generateGalaxy()
+
+/**
+ * Galaxy GUI
+ */
+gui.add(parameters, 'count', 100, 1000000, 100).onFinishChange(generateGalaxy)
+gui.add(parameters, 'size', 0.001, 0.1, 0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'radius', 0.01, 200, 0.01).onFinishChange(generateGalaxy)
+gui.add(parameters, 'branches', 2, 20, 1).onFinishChange(generateGalaxy)
+gui.add(parameters, 'spin', -5, 5, 0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'randomness', 0, 2, 0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'randomnessPower', 1, 10, 0.001).onFinishChange(generateGalaxy)
+gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
+gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
 
 /**
  * Sizes
@@ -145,9 +150,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 2
+camera.position.z = 3
 scene.add(camera)
 
 // Controls
