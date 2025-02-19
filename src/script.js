27bfd4364925a9de827cbd100c874816c8a5b0b8
Dramn <cepowiczjakub@gmail.com>
2025-02-19 21:33:57 +0100

    raycaster-base

--- src/script.js
+++ src/script.js
@@ -1,120 +1,44 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
-import gsap from 'gsap'
-
-// DEBUG
-const gui = new GUI({
-    width: 300,
-    title: 'GUI'
-    // closeFolders: true
-})
-// gui.close()
-// gui.hide()
-
-window.addEventListener('keydown', (e) => {
-    if(e.key == 'g'){
-        gui.show(gui._hidden)
-    }
-})
-
-const debugObject = {}
-
-
-
-
-
-// TEXTURES
-const loadingManager = new THREE.LoadingManager()
-
-// loadingManager.onStart = () =>{
-//     console.log('onStart')
-// }
-// loadingManager.onLoad = () =>{
-//     console.log('onLoad')
-// }
-// loadingManager.onProgress = () =>{
-//     console.log('onProgress')
-// }
-// loadingManager.onError = () =>{
-//     console.log('onError')
-// }
-
-const textureLoader = new THREE.TextureLoader(loadingManager)
-
-// COLOR
-const doorColorTexture = textureLoader.load('/textures/door/basecolor.jpg')
-    doorColorTexture.colorSpace = THREE.SRGBColorSpace
-    doorColorTexture.rotation = Math.PI * 0.25
-    doorColorTexture.center.x = 0.5
-    doorColorTexture.center.y = 0.5
-
-
-// // ALPHA
-// const doorAlphaTexture = textureLoader.load('/textures/door/opacity.jpg')
-//     doorAlphaTexture.colorSpace = THREE.SRGBColorSpace
-// // HEIGHT
-// const doorHeightTexture = textureLoader.load('/textures/door/height.png')
-//     doorHeightTexture.colorSpace = THREE.SRGBColorSpace
-// // NORMAL
-// const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
-//     doorNormalTexture.colorSpace = THREE.SRGBColorSpace
-// // ROUGHNESS
-// const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
-//     doorRoughnessTexture.colorSpace = THREE.SRGBColorSpace
-// // AMBIENT OCCLUSION
-// const doorAmbientOcclusion = textureLoader.load('/textures/door/ambientOcclusion.jpg')
-// doorAmbientOcclusion.colorSpace = THREE.SRGBColorSpace
-
-
-
 
 /**
  * Base
  */
+// Debug
+const gui = new GUI()
+
 // Canvas
-const canvas = document.querySelector('canvas.vite')
+const canvas = document.querySelector('canvas.webgl')
+
 // Scene
 const scene = new THREE.Scene()
 
+/**
+ * Objects
+ */
+const object1 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+object1.position.x = - 2
 
+const object2 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
 
-//OBJECT
-    /**COLOR**/debugObject.color = "#42c286"
+const object3 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+object3.position.x = 2
 
-const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-// console.log(geometry.attributes.uv)
-// const material = new THREE.MeshBasicMaterial({color: debugObject.color, wireframe: true})
-const material = new THREE.MeshBasicMaterial({map: doorColorTexture})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+scene.add(object1, object2, object3)
 
-
-
-const cubeGUI = gui.addFolder('Simple_cube')
-// cubeGUI.close()
-
-// DEBUG
-cubeGUI.add(mesh.position, 'y', -3, 3, 0.01).name('elevation')
-cubeGUI.add(mesh, 'visible')
-cubeGUI.add(material, 'wireframe')
-// cubeGUI.addColor(debugObject, 'color')
-//     .onFinishChange(()=>
-//     {
-//         material.color.set(debugObject.color)
-//     })
-
-debugObject.spin = () =>{
- gsap.to(mesh.rotation, { y: mesh.rotation.y + Math.PI * 2 })
-}
-cubeGUI.add(debugObject, 'spin')
-
-// debugObject.subdivision = 2
-// cubeGUI.add(debugObject, 'subdivision', 1, 20, 1)
-//     .onFinishChange(()=>{
-//         mesh.geometry.dispose()
-//         mesh.geometry = new THREE.BoxGeometry(1, 1, 1, debugObject.subdivision, debugObject.subdivision, debugObject.subdivision)
-//     })
+/**
+ * Raycaster
+ */
 
 
 /**
@@ -145,9 +69,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 2
+camera.position.z = 3
 scene.add(camera)
 
 // Controls
